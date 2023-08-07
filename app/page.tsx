fbd16f24d73dc6a2783d6fef0a8c06b30cc6f3b6
leo <leohthom@gmail.com>
2023-08-08 04:17:22 +0800

    Added Search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,8 @@
 "use client";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { BsSearch } from "react-icons/bs";
 import Image from "next/image";
+import Search from "@/components/Search";
 
 export default function Home() {
-  const [city, setCity] = useState("");
-  const [weather, setWeather] = useState({});
-  const [loading, setLoading] = useState(false);
-
-  const fetchWeather = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    setLoading(true);
-    axios.get(url).then((res) => {
-      setWeather(res.data);
-      console.log(res.data);
-      setLoading(false);
-    });
-    setCity("");
-    setLoading(false);
-  };
-
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=hongkong&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {/* Overlay */}
@@ -34,6 +14,8 @@ export default function Home() {
         alt="gradient background"
         className="object-cover"
       />
+      {/* Search */}
+      <Search />
     </main>
   );
 }
