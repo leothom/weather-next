9ab09584b8c4efe8685d450787fdf51b427e3662
leo <leohthom@gmail.com>
2023-08-08 03:52:25 +0800

    Added bg gradient image

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BsSearch } from "react-icons/bs";
+import Image from "next/image";
 
 export default function Home() {
   const [city, setCity] = useState("");
@@ -24,7 +25,13 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={fetchWeather}>Fetch data</button>
+      {/* Background image */}
+      <Image
+        src="https://images.unsplash.com/photo-1620641788421-7a1c342ea42e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1674&q=80"
+        layout="fill"
+        alt="gradient background"
+        className="object-cover"
+      />
     </main>
   );
 }
